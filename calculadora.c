6b177edbc16cc0057d8d314f63e9736f8ad2c64e
guilherme-schulke <58240853+guilherme-schulke@users.noreply.github.com>
2020-01-16 17:47:08 -0300

    Add files via upload

---
+++ calculadora.c
@@ -0,0 +1,97 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+#include <locale.h>
+
+int main(void){
+
+setlocale(LC_ALL, "portuguese");
+
+char escolha;
+float numero1, numero2, adicao, subtracao, divisao, multiplicacao;
+
+printf("\n°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°\n");
+printf("°\n°\t bem-vindo a calculadora         °\n");
+printf("°\n°\t tecle 1 para Adição             °\n");
+printf("°\n°\t tecle 2 para Subtração          °\n");
+printf("°\n°\t tecle 3 para divisão            °\n");
+printf("°\n°\t tecle 4 para Multiplicação      °\n");
+printf("°\n°\t tecle 0 para sair               °\n");
+printf("\n°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°\n");
+printf("\n\nescolha:\t\n"); scanf("%c", &escolha);
+
+system("cls");
+
+
+switch(escolha)
+{
+
+case '1':
+    printf("\nescolha um numero\n");
+    scanf("%f", &numero1);
+    printf("\nescolha outro numero\n");
+    scanf("%f", &numero2);
+
+    adicao=numero1+numero2;
+
+    system("cls");
+
+    printf("o resultado da soma é: %1.f", adicao);
+
+ break;
+
+case '2':
+
+    printf("\nescolha um numero\n");
+    scanf("%f", &numero1);
+    printf("\nescolha outro numero\n");
+    scanf("%f", &numero2);
+
+    subtracao=numero1-numero2;
+
+    system("cls");
+
+    printf("o resultado da soma é: %1.f", subtracao);
+
+;break;
+
+case '3':
+
+    printf("\nescolha um numero\n");
+    scanf("%f", &numero1);
+    printf("\nescolha outro numero\n");
+    scanf("%f", &numero2);
+
+    divisao=numero1/numero2;
+
+    system("cls");
+
+    printf("o resultado da soma é: %1.f", divisao);
+
+;break;
+
+case '4':
+
+    printf("\nescolha um numero\n");
+    scanf("%f", &numero1);
+    printf("\nescolha outro numero\n");
+    scanf("%f", &numero2);
+
+    multiplicacao=numero1*numero2;
+
+    system("cls");
+
+    printf("o resultado da soma é: %1.f", multiplicacao);
+
+;break;
+
+case '0': exit(0); break;
+
+default:printf("\nSYNTAX ERROR\n");
+}
+
+
+
+    system("pause>nul");
+    return 0;
+}
